f7c766114c3d32a5c7be9689622958221d9ec23c
Servet Simsek <i13090@student.htlwrn.ac.at>
2018-10-01 16:24:11 +0200

    ColorPong auf git stellen

--- ColorPong/GameScene.swift
+++ ColorPong/GameScene.swift
@@ -9,81 +9,189 @@
 import SpriteKit
 import GameplayKit
 
+extension UIColor {
+    var name: String? {
+        switch self {
+        case UIColor.white: return "White"
+        case UIColor.red: return "Red"
+        case UIColor.green: return "Green"
+        case UIColor.blue: return "Blue"
+        case UIColor.cyan: return "Cyan"
+        case UIColor.brown: return "Brown"
+        default: return nil
+        }
+    }
+}
+
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball = SKSpriteNode()
+    var lmittig = false
+    var rmittig = false
+    var currentBallColor = "White"
+    var currentCPColor = "White"
+    
+    var Checkpoint1 = SKSpriteNode()
+    var Checkpoint2 = SKSpriteNode()
+    
+    var Colors: [String] = ["Blue","Green", /*"Red",*/ "White"/*, "Brown", "Cyan"*/]
+    
+    var counter = 0
+    
+    var scoreLabel = SKLabelNode()
+    var scoreInt = 0
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+        scoreLabel = self.childNode(withName: "Score") as! SKLabelNode
+        ball.position.x = 335
+        ball.position.y = -262.07
+        let colorize = SKAction.colorize(with: changeBallColor(), colorBlendFactor: 1, duration: 0)
+        ball.run(colorize)
+        Checkpoint1 = self.childNode(withName: "Checkpoint1") as! SKSpriteNode
+        Checkpoint2 = self.childNode(withName: "Checkpoint2") as! SKSpriteNode
+        Checkpoint1.color = .green
+        Checkpoint2.color = .blue
+    }
+    
     
     override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        //ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        border.restitution = 1
+        border.friction = 0
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        self.physicsBody = border
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
+        counter = counter + 1
+        if(counter == 100){
+            Checkpoint1.color = changeCPColor()
+            Checkpoint2.color = changeCPColor()
+            counter = 0
+        }
+        
         // Called before each frame is rendered
+        if(ball.position.x < 0 && ball.position.x > -100){
+            lmittig = true
+        }else if(ball.position.x > 0 && ball.position.x < 100){
+            rmittig = true
+        }
+        
+        if(rmittig){
+            if(currentBallColor == Checkpoint1.color.name){
+                ball.position.x = -335
+                ball.position.y = 94.047
+                ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                rmittig = false
+                scoreInt = scoreInt + 1
+                scoreLabel.text = String(scoreInt)
+                let colorize = SKAction.colorize(with: changeBallColor(), colorBlendFactor: 1, duration: 0)
+                ball.run(colorize)
+            }else{
+                ball.position.x = 335
+                ball.position.y = -262.07
+                ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                rmittig = false
+                scoreInt = 0
+                scoreLabel.text = String(scoreInt)
+                let colorize = SKAction.colorize(with: changeBallColor(), colorBlendFactor: 1, duration: 0)
+                ball.run(colorize)
+            }
+        }
+        
+        if(lmittig){
+            if(currentBallColor == Checkpoint2.color.name){
+                ball.position.x = 335
+                ball.position.y = 407.93
+                ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                lmittig = false
+                scoreInt = scoreInt + 1
+                scoreLabel.text = String(scoreInt)
+                let colorize = SKAction.colorize(with: changeBallColor(), colorBlendFactor: 1, duration: 0)
+                ball.run(colorize)
+            }else{
+                ball.position.x = 335
+                ball.position.y = -262.07
+                ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                lmittig = false
+                scoreInt = 0
+                scoreLabel.text = String(scoreInt)
+                let colorize = SKAction.colorize(with: changeBallColor(), colorBlendFactor: 1, duration: 0)
+                ball.run(colorize)
+            }
+        }
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            if(location.x < 0){
+                ball.physicsBody?.applyImpulse(CGVector(dx: -10, dy: 10))
+            }else if(location.x > 0){
+                ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
+            }
+            
+        }
+    }
+    
+    func changeBallColor() -> UIColor{
+        let index = Int(arc4random_uniform(UInt32(Colors.count - 1)))
+        
+        
+        switch Colors[index] {
+        case "Blue":
+            currentBallColor = "Blue"
+            return .blue
+        /*case "Brown":
+            currentBallColor = "Brown"
+            return .brown*/
+        case "Green":
+            currentBallColor = "Green"
+            return .green
+        /*case "Red":
+            currentBallColor = "Red"
+            return .red
+        case "Cyan":
+            currentBallColor = "Cyan"
+            return .cyan*/
+        default:
+            return .white
+        }
+    }
+    
+    func changeCPColor() -> UIColor{
+        let index = Int(arc4random_uniform(UInt32(Colors.count)))
+        
+        
+        switch Colors[index] {
+        case "Blue":
+            currentCPColor = "Blue"
+            return .blue
+            /*case "Brown":
+             currentCPColor = "Brown"
+             return .brown*/
+        case "Green":
+            currentCPColor = "Green"
+            return .green
+            /*case "Red":
+             currentCPColor = "Red"
+             return .red
+             case "Cyan":
+             currentCPColor = "Cyan"
+             return .cyan*/
+        default:
+            return .white
+        }
+    }
+    
+    
 }
